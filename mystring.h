9010578772c44dbf059b28404c0f326c2917a41f
Mike-415 <mgonz160@mail.ccsf.edu>
2018-11-19 12:40:43 -0800

    Added comments to the header file

--- mystring.h
+++ mystring.h
@@ -1,8 +1,65 @@
-//
-// Created by Michael Gonzalez on 11/17/18.
-//
-#include <iostream>
+/*
+    Name:       Michael Gonzalez
+    Class:      cs110b
+    Instructor: Dave Harden
+    File:       mystring.h
 
+    The mystring class is used to make working with sequences of characters a little more
+    convenient and less error-prone than handling raw c-strings.
+
+    The following functions are available:
+
+    mystring();
+      post: Constructs a mystring object with an empty string for the myString data member.
+
+
+    mystring(const char *inMyString);
+      post: Constructs a mystring object with a c-string argument for the myString data member.
+
+    mystring(const mystring& otherString);
+      post: Constructs a hard copy of a mystring object with another mystring object
+            that provides the myString data member
+
+    ~mystring();
+      post: deallocates any object within the stack, usually when
+            execution reaches the end of the function body.
+
+    int length() const;
+      post: returns the number of characters contained within the myString data member.
+
+    friend std::ostream& operator<<(std::ostream& leftOS, const mystring& rightOperand);
+      post: returns an output stream that contains the myString data member.
+
+    mystring operator=(const mystring& right);
+      post: returns a hard copy of the right mystring object
+
+
+    char operator[](int index) const;
+      post: returns the character within the index of the myString char array.
+
+    char& operator[](int index);
+      post: returns a reference to the left-side operand in order to assign
+            it a value.
+
+    friend bool operator<(const mystring& leftOperand, const mystring& rightOperand);
+      post: Returns true if the left operand is less than to the right operand.
+
+    friend bool operator>(const mystring& leftOperand, const mystring& rightOperand);
+      post: Returns true if the left operand is greater than to the right operand.
+
+    friend bool operator<=(const mystring& leftOperand, const mystring& rightOperand);
+      post: Returns true if the left operand is less than or equal to the right operand.
+
+    friend bool operator>=(const mystring& leftOperand, const mystring& rightOperand);
+      post: Returns true if the left operand is greater than or equal to the right operand.
+
+    friend bool operator!=(const mystring& leftOperand, const mystring& rightOperand);
+      post: Returns true if the left operand is not equal to the right operand.
+
+    friend bool operator==(const mystring& leftOperand, const mystring& rightOperand);
+      post: Returns true if the left operand is equal to the right operand.
+*/
+#include <iostream>
 #ifndef MYSTRING_MYSTRING_H
 #define MYSTRING_MYSTRING_H
 namespace cs_mystring{
@@ -13,17 +70,16 @@ namespace cs_mystring{
         mystring(const mystring& otherString);
         ~mystring();
         int length() const;
-        friend std::ostream& operator<<(std::ostream& leftOS, const mystring& rightOperand);
         mystring operator=(const mystring& right);
         char operator[](int index) const;
         char& operator[](int index);
+        friend std::ostream& operator<<(std::ostream& leftOS, const mystring& rightOperand);
         friend bool operator<(const mystring& leftOperand, const mystring& rightOperand);
         friend bool operator>(const mystring& leftOperand, const mystring& rightOperand);
         friend bool operator<=(const mystring& leftOperand, const mystring& rightOperand);
         friend bool operator>=(const mystring& leftOperand, const mystring& rightOperand);
         friend bool operator!=(const mystring& leftOperand, const mystring& rightOperand);
         friend bool operator==(const mystring& leftOperand, const mystring& rightOperand);
-
     private:
         char *myString;
     };
